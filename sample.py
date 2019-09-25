b36e294914892291d1a05b50fa37f3e7c89b22a9
FloatingPoint64 <gmagitype@gmail.com>
2019-09-25 11:26:51 +0900

    first commit from VSCode

--- sample.py
+++ sample.py
@@ -4,5 +4,4 @@ import datetime
 if __name__ == "__main__":
     now = datetime.datetime.now()
     now_str = now.strftime("%Y/%m/%d %H-%M-%S")
-    print(f"{now_str}: Hello world!!")
-
+    print(f"{now_str}: Hello new world!!")
